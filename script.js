993f0331e18788cbb44cb0c3a4e0e0eec4568d2a
Hraponssi <hraponssi@gmail.com>
2023-03-20 22:22:23 +0200

    Working gameplay, styling

--- script.js
+++ script.js
@@ -8,28 +8,38 @@ const startButton = document.querySelector(".playStart");
 const standButton = document.querySelector(".playStand");
 const hitButton = document.querySelector(".playHit");
 
-const cardDisplay = document.createElement('p');
-const cardTotalDisplay = document.createElement('p');
-const resultDisplay = document.createElement('p');
+const gameSection = document.querySelector(".gameSection");
+const gameContent = document.querySelector(".gameContent");
 
-cardDisplay.textContent = "Cards: ";
+const cardDisplay = document.querySelector(".cardDisplay");
+const cardTotalDisplay = document.querySelector(".cardTotalDisplay");
+const resultDisplay = document.querySelector(".resultDisplay");
+const dealerDisplay = document.querySelector(".dealerDisplay");
+const dealerTotalDisplay = document.querySelector(".dealerTotalDisplay");
+const dealerResultDisplay = document.querySelector(".dealerResultDisplay");
+const winnerDisplay = document.querySelector(".winnerDisplay");
+
+cardDisplay.textContent = "Your cards: ";
 cardTotalDisplay.textContent = "Total value: ";
 resultDisplay.textContent = "";
-
-document.body.appendChild(cardDisplay);
-document.body.appendChild(cardTotalDisplay);
-document.body.appendChild(resultDisplay);
+dealerDisplay.textContent = "The dealers hand: ";
+dealerTotalDisplay.textContent = "Dealer value: ";
+dealerResultDisplay.textContent = "";
+winnerDisplay.textContent = "";
 
 let userName = "";
 let gamesPlayed = 0;
+let winCount = 0;
 let playing = false;
 let cards = [];
+let dcards = [];
+let showDealerHand = false;
 
 initialise();
 
 function initialise() {
     nameDisplay.textContent = "Your name: ";
-    gameCount.textContent = "Game count: 0";
+    gameCount.textContent = "Game count: 0, Win count: 0 (0%)";
     nameSubmit.addEventListener("click", checkName);
     startButton.addEventListener("click", startGame);
     standButton.addEventListener("click", playerStand);
@@ -47,15 +57,36 @@ function checkName() {
     const iName = String(nameField.value);
     userName = iName;
     nameDisplay.textContent = "Your name: " + userName;
+    gameSection.style.visibility = "visible";
+    gameContent.style.visibility = "visible";
 }
 
 function updateStats() {
-    gameCount.textContent = "Game count: " + gamesPlayed;
+    gameCount.textContent = "Game count: " + gamesPlayed + ", Win count: " + winCount + " (" + Math.floor(winCount*100/gamesPlayed) + "%)";
 }
 
 function updateCards() {
-    cardDisplay.textContent = "Cards: " + cards.toString();
-    cardTotalDisplay.textContent = "Total value: " + cardsValue();
+    cardDisplay.textContent = "Your cards: " + cards.toString();
+    cardTotalDisplay.textContent = "Total value: " + cardsValue(cards);
+    if (dcards.length === 0) {
+        dealerDisplay.textContent = "The dealers hand: ";
+        dealerTotalDisplay.textContent = "Dealer value: ";
+    } else if (showDealerHand) {
+        dealerDisplay.textContent = "The dealers hand: " + dcards.toString();
+        dealerTotalDisplay.textContent = "Dealer value: " + cardsValue(dcards);
+    } else {
+        let dtext = "The dealers hand: ";
+        let first = true;
+        for (dcard of dcards) {
+            if (first) {
+                dtext += dcard;
+                first = false;
+            } else {
+                dtext += ", x"
+            }
+        }
+        dealerDisplay.textContent = dtext;
+    }
 }
 
 function startGame() {
@@ -70,15 +101,22 @@ function startGame() {
     hitButton.disabled = false;
     startButton.disabled = true;
     cards = [];
+    dcards = [];
+    showDealerHand = false;
+    dealerDisplay.textContent = "The dealers hand: ";
+    dealerResultDisplay.textContent = "";
     resultDisplay.textContent = "";
-    newCard();
-    newCard();
+    winnerDisplay.textContent = "";
+    newPlayerCard();
+    newPlayerCard();
+    newDealerCard();
+    newDealerCard();
 }
 
-function cardsValue() {
+function cardsValue(cardset) {
     totalValue = 0;
     // The face cards aren't worth over 10
-    cards.forEach(card => totalValue += Math.min(card, 10));
+    cardset.forEach(card => totalValue += Math.min(card, 10));
     // Other method
     //for (index in cards) {
     //    totalValue += Math.min(cards[index], 10);
@@ -86,32 +124,87 @@ function cardsValue() {
     return totalValue;
 }
 
-function checkHand() {
-    totalValue = cardsValue();
-    if (totalValue > 21) {
-        alert("Card value over 21!");
-        endGame("You went over 21!");
+function cardsOver(cardset) {  //Return true if hand is over 21
+    totalValue = cardsValue(cardset);
+    if (totalValue > 21){
+        return true;
     }
+    return false;
 }
 
 function newCard() {
-    let rng = Math.floor(Math.random() * 14);
-    cards.push(rng);
+    let rng = Math.floor(Math.random() * 13)+1;
+    return rng;
+}
+
+function newDealerCard() {
+    let ncard = newCard();
+    dcards.push(ncard);
     updateCards();
 }
 
+function newPlayerCard() {
+    let ncard = newCard();
+    cards.push(ncard);
+    updateCards();
+}
+
+function checkWinner() {
+    let dplaying = true;
+    while (dplaying) {
+        let handval = cardsValue(dcards);
+        if (handval < 17) {
+            newDealerCard();
+        } else {
+            if (cardsOver(dcards)) {
+                dealerResultDisplay.textContent = "The dealer went over 21!";
+                if (cardsOver(cards)) return "dealer";
+                return userName;
+            } else {
+                let phand = cardsValue(cards);
+                if (cardsOver(cards)) {
+                    return "dealer";
+                } else if (handval > phand) {
+                    return "dealer";
+                } else if (handval === phand) {
+                    return "push"
+                } else {
+                    return userName;
+                }
+            }
+            dplaying = false;
+        }
+    }
+}
+
 function endGame(reason) {
+    showDealerHand = true;
+    updateCards();
+    let winner = checkWinner();
+    winnerDisplay.textContent = "Winner: " + winner;
+    if (winner === userName) {
+        winCount++;
+        winnerDisplay.style.color = "green";
+    } else if (winner === "dealer") {
+        winnerDisplay.style.color = "red";
+    } else {
+        winnerDisplay.style.color = "yellow";
+    }
     disableGame();
     resultDisplay.textContent = reason;
+    updateStats();
 }
 
 function playerStand() {
     //alert("You stand");
-    endGame("You stood at " + cardsValue());
+    endGame("You stood at " + cardsValue(cards));
 }
 
 function playerHit() {
     //alert("You hit");
-    newCard();
-    checkHand();
+    newPlayerCard();
+    if (cardsOver(cards)) {
+        //alert("Card value over 21!");
+        endGame("You went over 21! You had: " + cardsValue(cards));
+    }
 }
